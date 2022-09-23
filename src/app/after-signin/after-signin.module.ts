db6a9b635e409daf98ebeba2b2bcb703fe8a4330
Kacper <69423945+Kacper-adamiak@users.noreply.github.com>
2022-09-23 12:55:52 +0200

    v1.0.1
    Created account page
    Created courses page

--- src/app/after-signin/after-signin.module.ts
+++ src/app/after-signin/after-signin.module.ts
@@ -4,6 +4,7 @@ import { AfterSigninComponent } from './after-signin.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RouterModule } from '@angular/router';
 import { AccountPageComponent } from './account-page/account-page.component';
+import { CoursesPageComponent } from './courses-page/courses-page.component';
 
 
 
@@ -11,7 +12,8 @@ import { AccountPageComponent } from './account-page/account-page.component';
   declarations: [
     AfterSigninComponent,
     HomePageComponent,
-    AccountPageComponent
+    AccountPageComponent,
+    CoursesPageComponent
   ],
   imports: [
     CommonModule,
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountPageComponent } from './after-signin/account-page/account-page.component';
 import { AfterSigninComponent } from './after-signin/after-signin.component';
+import { CoursesPageComponent } from './after-signin/courses-page/courses-page.component';
 import { HomePageComponent } from './after-signin/home-page/home-page.component';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
@@ -11,8 +12,9 @@ const routes: Routes = [
   {path: "", component: AppComponent, children: [
     {path: "", component: AfterSigninComponent, children: [
       {path: "home", component: HomePageComponent},
-      {path: '',   component: HomePageComponent},
       {path: 'account', component: AccountPageComponent},
+      {path: 'courses', component: CoursesPageComponent},
+      {path: '',   component: HomePageComponent},
     ]},
     {path: "auth", component: AuthComponent, children: [
       {path: "signin", component: SigninPageComponent}
