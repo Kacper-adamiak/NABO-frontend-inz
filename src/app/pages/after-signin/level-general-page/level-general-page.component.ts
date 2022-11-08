aec2ff8221d22f8aeace07aa22dee6546cae8a41
Kacper <69423945+Kacper-adamiak@users.noreply.github.com>
2022-11-08 11:41:34 +0100

    Added Flashcard

---
+++ src/app/pages/after-signin/level-general-page/level-general-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-level-general-page',
+  templateUrl: './level-general-page.component.html',
+  styleUrls: ['./level-general-page.component.scss']
+})
+export class LevelGeneralPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
