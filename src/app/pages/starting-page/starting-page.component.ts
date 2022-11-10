f3f8ad6d9a337b77c0c14207b870d2458488778d
Kacper <69423945+Kacper-adamiak@users.noreply.github.com>
2022-11-10 08:37:13 +0100

    Modified StartingPageComponent

--- src/app/pages/starting-page/starting-page.component.ts
+++ src/app/pages/starting-page/starting-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DialogService} from "../../services/dialog/dialog.service";
 
 @Component({
   selector: 'app-starting-page',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogService: DialogService) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog() {
+    const dialog = this.dialogService.openUploadImage()
   }
 
 }
