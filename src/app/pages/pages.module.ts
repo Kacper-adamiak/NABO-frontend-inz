cc636c8e72c21089f0e94ae2f222b7a3e65dd064
Kacper <69423945+Kacper-adamiak@users.noreply.github.com>
2022-11-10 21:40:40 +0100

    Fixed

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuthModule} from "./auth/auth.module";
+import {AfterSigninModule} from "./after-signin/after-signin.module";
+import {StartingPageComponent} from "./starting-page/starting-page.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "../app-routing.module";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MaterialExampleModule} from "../../material.module";
+
+
+@NgModule({
+  declarations: [
+    StartingPageComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    AuthModule,
+    AfterSigninModule,
+  ]
+})
+export class PagesModule { }
