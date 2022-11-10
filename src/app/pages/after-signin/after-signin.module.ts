98d4c092931733ff6409f0df37a5e0257dbe3dde
Kacper <69423945+Kacper-adamiak@users.noreply.github.com>
2022-11-10 08:37:40 +0100

    Modified AfterSigninModule

--- src/app/pages/after-signin/after-signin.module.ts
+++ src/app/pages/after-signin/after-signin.module.ts
@@ -1,20 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AfterSigninComponent } from './after-signin.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { RouterModule } from '@angular/router';
-import { AccountPageComponent } from './account-page/account-page.component';
-import { CoursesPageComponent } from './courses-page/courses-page.component';
-import { CourseGeneralPageComponent, DialogOverviewExampleDialog } from './course-general-page/course-general-page.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AfterSigninComponent} from './after-signin.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {RouterModule} from '@angular/router';
+import {AccountPageComponent} from './account-page/account-page.component';
+import {CoursesPageComponent} from './courses-page/courses-page.component';
+import {
+  CourseGeneralPageComponent,
+  DialogOverviewExampleDialog
+} from './course-general-page/course-general-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MaterialExampleModule } from 'src/material.module';
-import { NewCourseDialogComponent } from './courses-page/new-course-dialog/new-course-dialog.component';
-import { LevelsPageComponent } from './levels-page/levels-page.component';
-import {MatPaginatorModule} from "@angular/material/paginator";
-import { NewLevelDialogComponent } from './levels-page/new-level-dialog/new-level-dialog.component';
+import {MaterialExampleModule} from 'src/material.module';
+import {NewCourseDialogComponent} from './courses-page/new-course-dialog/new-course-dialog.component';
+import {LevelsPageComponent} from './levels-page/levels-page.component';
+import {NewLevelDialogComponent} from './levels-page/new-level-dialog/new-level-dialog.component';
 import {LevelGeneralPageComponent} from "./level-general-page/level-general-page.component";
-import { EditedLevelDialogComponent } from './level-general-page/edited-level-dialog/edited-level-dialog.component';
-
+import {EditedLevelDialogComponent} from './level-general-page/edited-level-dialog/edited-level-dialog.component';
+import {FlashcardsPageComponent} from './flashcards-page/flashcards-page.component';
+import {TestQuestionsPageComponent} from './test-questions-page/test-questions-page.component';
+import {ExercisesPageComponent} from './exercises-page/exercises-page.component';
+import {NewFlashcardDialogComponent} from './flashcards-page/new-flashcard-dialog/new-flashcard-dialog.component';
+import {FlashcardGeneralPageComponent} from './flashcard-general-page/flashcard-general-page.component';
+import {NewExerciseDialogComponent} from './exercises-page/new-exercise-dialog/new-exercise-dialog.component';
 
 
 @NgModule({
@@ -29,7 +36,13 @@ import { EditedLevelDialogComponent } from './level-general-page/edited-level-di
     LevelsPageComponent,
     NewLevelDialogComponent,
     LevelGeneralPageComponent,
-    EditedLevelDialogComponent
+    EditedLevelDialogComponent,
+    FlashcardsPageComponent,
+    TestQuestionsPageComponent,
+    ExercisesPageComponent,
+    NewFlashcardDialogComponent,
+    FlashcardGeneralPageComponent,
+    NewExerciseDialogComponent
   ],
   imports: [
     CommonModule,
